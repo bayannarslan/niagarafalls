0871749c26c75b2be0ed3e7663a4016a4ea74406
bayannarslan <106767817+bayannarslan@users.noreply.github.com>
2023-12-27 16:05:23 -0500

    Add files via upload

---
+++ L3Demo/L3Demo/L3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L3DemoApp.swift
+//  L3Demo
+//
+//  Created by Bayann Arslan on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct L3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
